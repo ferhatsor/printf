f35ee6098e2949f58e9df98ee752554dab0fa879
ferhatsor <abdul.sor@stud.thga.de>
2024-06-10 17:02:44 +0200

    ansi\ color .c

--- ansi\ color .c
+++ ansi\ color .c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
 int main(void){
-	printf("\033[47;31m .___. \033[0m\n");
-	printf("\033[47;31m {o,o} \033[0m\n");
-	printf("\033[47;31m /)__) \033[0m\n");
-	printf("\033[47;31m -´-´- \033[0m\n");
+        printf("\033[36m  *****  \033[0m\n");
+        printf("\033[36m *     * \033[0m\n");
+        printf("\033[36m*  o o  *\033[0m\n");
+        printf("\033[36m*   ^   *\033[0m\n");
+        printf("\033[36m*  '-'  *\033[0m\n");
+        printf("\033[36m *     * \033[0m\n");
+        printf("\033[36m  *****  \033[0m\n");
 	return 0;
 }
