37d509ab3bf1fdd45349653895b92f2dfca619ab
ferhatsor <abdul.sor@stud.thga.de>
2024-06-10 16:59:08 +0200

    ansi

---
+++ ansi\ color .c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+	printf("\033[47;31m .___. \033[0m\n");
+	printf("\033[47;31m {o,o} \033[0m\n");
+	printf("\033[47;31m /)__) \033[0m\n");
+	printf("\033[47;31m -´-´- \033[0m\n");
+	return 0;
+}
